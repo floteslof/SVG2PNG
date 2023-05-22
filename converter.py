8f13792eb54ed3cf3f58002805f8fbaa6af9a63f
root <root@ThinkbookFK>
2023-05-22 21:23:30 +0200

    Changes

--- converter.py
+++ converter.py
@@ -1,10 +1,13 @@
 import os
 from cairosvg import svg2png
 
+WIDTH=128
+HEIGHT=128
+
 for filename in os.listdir('input'):
     f = open(f'input/{filename}', 'rb')
 
     content = f.read()
     name = f.name.split('/')[1].split('.')[0]
 
-    svg2png(bytestring=content, write_to=f'output/{name}.png')
+    svg2png(bytestring=content, write_to=f'output/{name}.png', parent_width=WIDTH, parent_height=HEIGHT)
